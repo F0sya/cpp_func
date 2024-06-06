b925e9c4a7628e0e493940353613d0323fd26a95
F0sya <nikitachmyria@gmail.com>
2024-06-06 19:52:09 +0300

    Fifth practice exercise is done.

--- cpp_func/main.cpp
+++ cpp_func/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
 
+
+int max(int x, int y) {
+	int result;
+	(x < y) ? result = y : result = x;
+	return result;
+}
+int main() {
+	int first_number; cout << "Enter an first number:"; cin >> first_number;
+	int second_number; cout << "Enter an second number:"; cin >> second_number;
+
+	cout << max(first_number, second_number) << endl;
+	return 0;
 }
