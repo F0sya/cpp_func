218310b0e973e6af420d87499d63d6a013849d44
F0sya <nikitachmyria@gmail.com>
2024-06-06 19:38:23 +0300

    Second practice exercise is done.

--- cpp_func/main.cpp
+++ cpp_func/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {
 
+int factorial(int x) {
+	int multiplier = 1;
+	for (int i = 1; i <= x; i++) {
+		multiplier *= i;
+	}
+	return multiplier;
+}
+int main() {
+	int number; cout << "Enter a number:"; cin >> number;
+	
+	cout << factorial(number) << endl;
 }
