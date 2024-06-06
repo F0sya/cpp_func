940434adfaa71e2ad4ee6fcd8fa82cdd01989853
F0sya <nikitachmyria@gmail.com>
2024-06-06 19:32:28 +0300

    First practice exercise is done.

--- cpp_func/main.cpp
+++ cpp_func/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
 
+void rect(int x, int y) {
+	for (int i = 0; i < y; i++) {
+		for (int j = 0; j < x; j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+}
+int main() {
+	int width; cout << "Enter a width:"; cin >> width;
+	int height; cout << "Enter a height:"; cin >> height;
+
+	rect(width, height);
 }
