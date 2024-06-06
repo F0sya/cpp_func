bcd4133b344b6a127718562061834ab4a7561b99
F0sya <nikitachmyria@gmail.com>
2024-06-06 19:45:44 +0300

    Third practice exercise is done

--- cpp_func/main.cpp
+++ cpp_func/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+bool prime(int x) {
+	for (int i = 2; i < x; i++) {
+		if (x % i == 0) {
+			return false;
+		}
+	}
+	return true;
 
+}
+
+int main() {
+	int number; cout << "Enter a number:"; cin >> number;
+
+	if (prime(number) == true) {
+		cout << "Prime" << endl;
+	}
+	else {
+		cout << "Non prime" << endl;
+	}
+	return 0;
 }
