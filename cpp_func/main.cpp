e0275cb6a866d9f1b9021c00b83faba981705c38
F0sya <nikitachmyria@gmail.com>
2024-06-06 19:57:49 +0300

    Sixth practice exercise is done.

--- cpp_func/main.cpp
+++ cpp_func/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
 
+
+bool check(int x) {
+	bool result;
+	(x < 0) ? result = false : result = true;
+	return result;
+}
+int main() {
+	int number; cout << "Enter a number:"; cin >> number;
+	if (number == 0) {
+		cout << "It's zero" << endl;
+		return 0;
+	}
+	(check(number) == 1) ? cout << "True" << endl : cout << "False" << endl;
+	return 0;
 }
