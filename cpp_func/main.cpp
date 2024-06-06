4daa39916e4cae90f30c9e136ca55b7ff1e5b124
F0sya <nikitachmyria@gmail.com>
2024-06-06 19:48:01 +0300

    Fourth practice exercise is done.

--- cpp_func/main.cpp
+++ cpp_func/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
 
+
+int cube(int x) {
+	int multiplier = 1;
+	for (int i = 0; i < 3; i++) {
+		multiplier *= x;
+	}
+	return multiplier;
+}
+int main() {
+	int number; cout << "Enter a number:"; cin >> number;
+	
+	cout << cube(number) << endl;
+	return 0;
 }
