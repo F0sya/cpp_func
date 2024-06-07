12ee65a18e90eea07bf0508ed30f8ed30550044e
F0sya <nikitachmyria@gmail.com>
2024-06-08 01:29:10 +0300

    Third homework exercise is done.

--- cpp_func/main.cpp
+++ cpp_func/main.cpp
@@ -4,16 +4,34 @@ using namespace std;
 
 
 bool check(int x) {
-	bool result;
-	(x < 0) ? result = false : result = true;
-	return result;
+	int sum = 0;
+	for (int i = 1; i <= x / 2; i++) {
+		if (x % i == 0) {
+			sum += i;
+		}
+	}
+	return sum == x;
+}
+
+void perfect(int x, int y) {
+	int result = 0;
+	if (x > y) {
+		int temp = x;
+		x = y;
+		y = temp;
+	}
+	for (int i = x; i <= y; i++) {
+		if (check(i)) {
+			cout << i << " ";
+
+		}
+	}
+	cout << endl;
 }
 int main() {
-	int number; cout << "Enter a number:"; cin >> number;
-	if (number == 0) {
-		cout << "It's zero" << endl;
-		return 0;
-	}
-	(check(number) == 1) ? cout << "True" << endl : cout << "False" << endl;
+	int left_limit; cout << "Enter an first number:"; cin >> left_limit;
+	int right_limit; cout << "Enter an second number:"; cin >> right_limit;
+
+	perfect(left_limit, right_limit);
 	return 0;
 }
