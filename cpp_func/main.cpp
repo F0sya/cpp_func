aff2b7753a202cb998bd227f2f3298b583e06822
F0sya <nikitachmyria@gmail.com>
2024-06-08 13:46:42 +0300

    Fifth homework exercise is done.

--- cpp_func/main.cpp
+++ cpp_func/main.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
-
-
-bool check(int x) {
-	bool result;
-	(x < 0) ? result = false : result = true;
-	return result;
+bool check_luck(int x) {
+	int first_sum = 0, second_sum = 0;
+	first_sum = (x / 100000) + (x / 10000) % 10 + (x / 1000) % 10;
+	second_sum = (x % 10) + (x / 10) % 10 + (x / 100) % 10;
+	return first_sum == second_sum;
 }
 int main() {
 	int number; cout << "Enter a number:"; cin >> number;
-	if (number == 0) {
-		cout << "It's zero" << endl;
-		return 0;
-	}
-	(check(number) == 1) ? cout << "True" << endl : cout << "False" << endl;
+	do {
+		cout << "Enter a 6-digit number:"; cin >> number;
+	} while (number < 100000 || number >> 999999);
+	(check_luck(number) == 0) ? cout << "False" << endl : cout << "True" << endl;
+
 	return 0;
 }
